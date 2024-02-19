1a4e5e96b4a2d6040046b8092ab5354c61abc911
Lorenzo <lorenzosuarez1994@gmail.com>
2024-02-19 04:19:39 -0800

    UI + package name is updated

--- presentation/src/main/java/com/app/presentation/common/composables/ImageItem.kt
+++ presentation/src/main/java/com/app/presentation/common/composables/ImageItem.kt
@@ -2,14 +2,12 @@ package com.app.presentation.common.composables
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
